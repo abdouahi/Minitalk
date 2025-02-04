6e69705d163ed28bab7b7652a28c5b2da9a4df90
Abdessamad ouahidi <137984861+abdouahi@users.noreply.github.com>
2025-02-04 12:48:02 +0100

    Update client_bonus.c

--- client_bonus.c
+++ client_bonus.c
@@ -1,81 +1,65 @@
-#include "minitalk.h"
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
 
-static volatile sig_atomic_t	g_ack_received = 0;
+volatile sig_atomic_t g_ack_received = 0;
 
-static void	ack_handler(int sig)
-{
-	(void)sig;
-	g_ack_received = 1;
+void ack_handler(int sig) {
+    (void)sig;
+    g_ack_received = 1;
 }
 
-static void	send_bit(int pid, int bit)
-{
-	struct sigaction	sa;
-
-	sa.sa_handler = ack_handler;
-	sa.sa_flags = 0;
-	sigemptyset(&sa.sa_mask);
-	sigaction(SIGUSR1, &sa, NULL);
-	g_ack_received = 0;
-	if (bit)
-		kill(pid, SIGUSR2);
-	else
-		kill(pid, SIGUSR1);
-	while (!g_ack_received)
-		usleep(10);
+void send_bit(pid_t pid, char bit) {
+    int retry = 0;
+    do {
+        g_ack_received = 0;
+        if (kill(pid, bit ? SIGUSR1 : SIGUSR2) == -1) {
+            perror("kill");
+            exit(EXIT_FAILURE);
+        }
+        while (!g_ack_received && retry < 10) {
+            usleep(100);
+            retry++;
+        }
+    } while (!g_ack_received && retry < 10);
 }
 
-static void	send_unicode(int pid, wchar_t c)
-{
-	int	byte_count;
-	int	i;
-
-	if (c <= 0x7F)
-		byte_count = 1;
-	else if (c <= 0x7FF)
-		byte_count = 2;
-	else if (c <= 0xFFFF)
-		byte_count = 3;
-	else
-		byte_count = 4;
-	i = byte_count;
-	while (i--)
-	{
-		send_byte(pid, (c >> (6 * i)) & 0x3F | (i ? 0x80 : 0xFF >> (8 - byte_count)));
-	}
+void send_char(pid_t pid, char c) {
+    for (int i = 0; i < 8; i++) {
+        send_bit(pid, (c >> i) & 1);
+    }
 }
 
-void	send_message(int pid, char *str)
-{
-	int		i;
-	char	c;
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <server_pid> <message>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
 
-	while (*str)
-	{
-		i = 8;
-		c = *str++;
-		while (i--)
-		{
-			send_bit(pid, (c >> i) & 1);
-		}
-	}
-	i = 8;
-	while (i--)
-		send_bit(pid, 0);
-}
+    struct sigaction sa;
+    sa.sa_handler = ack_handler;
+    sa.sa_flags = 0;
+    sigemptyset(&sa.sa_mask);
 
-int	main(int argc, char **argv)
-{
-	int		pid;
-	char	*msg;
+    if (sigaction(SIGUSR1, &sa, NULL) == -1) {
+        perror("sigaction");
+        return EXIT_FAILURE;
+    }
 
-	if (argc != 3)
-	{
-		ft_printf("Usage: %s <PID> <message>\n", argv[0]);
-		return (1);
-	}
-	pid = ft_atoi(argv[1]);
-	msg = argv[2];
-	send_message(pid, msg);
-	return (0);
+    char *endptr;
+    long pid = strtol(argv[1], &endptr, 10);
+    if (*endptr != '\0' || pid <= 0) {
+        fprintf(stderr, "Invalid PID\n");
+        return EXIT_FAILURE;
+    }
+
+    char *message = argv[2];
+    while (*message) {
+        send_char(pid, *message++);
+    }
+    send_char(pid, '\0');
+
+    return EXIT_SUCCESS;
 }
