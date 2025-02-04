49d7316836f9f706e747b4ebc22bb739e59389b8
Abdessamad ouahidi <137984861+abdouahi@users.noreply.github.com>
2025-02-04 12:44:35 +0100

    Update server_bonus.c

--- server_bonus.c
+++ server_bonus.c
@@ -1,57 +1,80 @@
-#include "minitalk.h"
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-static void	config_signals(void)
-{
-	struct sigaction	sa;
+#define MAX_CLIENTS 10
 
-	sa.sa_sigaction = handle_signal;
-	sa.sa_flags = SA_SIGINFO;
-	sigemptyset(&sa.sa_mask);
-	sigaddset(&sa.sa_mask, SIGUSR1);
-	sigaddset(&sa.sa_mask, SIGUSR2);
-	if (sigaction(SIGUSR1, &sa, NULL) < 0)
-		exit(1);
-	if (sigaction(SIGUSR2, &sa, NULL) < 0)
-		exit(1);
+typedef struct {
+    pid_t pid;
+    char buffer[1024];
+    int buffer_index;
+    char current_char;
+    int bit_count;
+} client_state;
+
+static client_state clients[MAX_CLIENTS];
+
+static client_state *get_client(pid_t pid) {
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i].pid == pid) return &clients[i];
+        if (clients[i].pid == 0) {
+            clients[i].pid = pid;
+            clients[i].buffer_index = 0;
+            clients[i].bit_count = 0;
+            clients[i].current_char = 0;
+            return &clients[i];
+        }
+    }
+    return NULL;
 }
 
-int	main(void)
-{
-	ft_printf("Server PID: %d\n", getpid());
-	config_signals();
-	while (1)
-		pause();
-	return (0);
+static void signal_handler(int sig, siginfo_t *info, void *context) {
+    (void)context;
+    client_state *client = get_client(info->si_pid);
+    if (!client) return;
+
+    client->current_char |= (sig == SIGUSR1) << client->bit_count;
+    client->bit_count++;
+
+    if (client->bit_count == 8) {
+        if (client->buffer_index < sizeof(client->buffer) - 1) {
+            client->buffer[client->buffer_index++] = client->current_char;
+        }
+
+        if (client->current_char == '\0') {
+            write(STDOUT_FILENO, client->buffer, client->buffer_index - 1);
+            write(STDOUT_FILENO, "\n", 1);
+            client->pid = 0;
+        } else if (client->buffer_index >= sizeof(client->buffer) - 1) {
+            write(STDOUT_FILENO, client->buffer, sizeof(client->buffer) - 1);
+            write(STDOUT_FILENO, "\n", 1);
+            client->buffer_index = 0;
+        }
+
+        client->current_char = 0;
+        client->bit_count = 0;
+    }
+
+    kill(info->si_pid, SIGUSR1);
 }
 
-void	handle_signal(int sig, siginfo_t *info, void *context)
-{
-	static int				bit_count;
-	static unsigned char	c;
-	static int				client_pid;
-	static char				*message;
+int main(void) {
+    struct sigaction sa;
+    memset(clients, 0, sizeof(clients));
 
-	(void)context;
-	if (client_pid != info->si_pid)
-	{
-		client_pid = info->si_pid;
-		bit_count = 0;
-		c = 0;
-		message = ft_calloc(1, 1);
-	}
-	c = (c << 1) | (sig == SIGUSR2);
-	if (++bit_count == 8)
-	{
-		if (c == 0)
-		{
-			ft_printf("%s\n", message);
-			free(message);
-			message = NULL;
-		}
-		else
-			message = ft_strjoin_char(message, c);
-		bit_count = 0;
-		c = 0;
-	}
-	kill(client_pid, SIGUSR1);
+    sa.sa_sigaction = signal_handler;
+    sa.sa_flags = SA_SIGINFO;
+    sigemptyset(&sa.sa_mask);
+
+    if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1) {
+        perror("sigaction");
+        return EXIT_FAILURE;
+    }
+
+    printf("Server PID: %d\n", getpid());
+    while (1) pause();
+
+    return EXIT_SUCCESS;
 }
