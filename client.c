5405ba2e82347be34d4293716a11d22b2d907c42
Abdessamad ouahidi <137984861+abdouahi@users.noreply.github.com>
2025-02-04 12:45:11 +0100

    Update client.c

--- client.c
+++ client.c
@@ -1,45 +1,65 @@
-#include "minitalk.h"
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
 
-void	send_bit(int pid, int bit)
-{
-	if (bit)
-		kill(pid, SIGUSR2);
-	else
-		kill(pid, SIGUSR1);
-	usleep(100);
+volatile sig_atomic_t g_ack_received = 0;
+
+void ack_handler(int sig) {
+    (void)sig;
+    g_ack_received = 1;
 }
 
-void	send_message(int pid, char *str)
-{
-	int		i;
-	char	c;
-
-	while (*str)
-	{
-		i = 8;
-		c = *str++;
-		while (i--)
-		{
-			send_bit(pid, (c >> i) & 1);
-		}
-	}
-	i = 8;
-	while (i--)
-		send_bit(pid, 0);
+void send_bit(pid_t pid, char bit) {
+    int retry = 0;
+    do {
+        g_ack_received = 0;
+        if (kill(pid, bit ? SIGUSR1 : SIGUSR2) == -1) {
+            perror("kill");
+            exit(EXIT_FAILURE);
+        }
+        while (!g_ack_received && retry < 10) {
+            usleep(100);
+            retry++;
+        }
+    } while (!g_ack_received && retry < 10);
 }
 
-int	main(int argc, char **argv)
-{
-	int		pid;
-	char	*msg;
+void send_char(pid_t pid, char c) {
+    for (int i = 0; i < 8; i++) {
+        send_bit(pid, (c >> i) & 1);
+    }
+}
 
-	if (argc != 3)
-	{
-		ft_printf("Usage: %s <PID> <message>\n", argv[0]);
-		return (1);
-	}
-	pid = ft_atoi(argv[1]);
-	msg = argv[2];
-	send_message(pid, msg);
-	return (0);
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <server_pid> <message>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    struct sigaction sa;
+    sa.sa_handler = ack_handler;
+    sa.sa_flags = 0;
+    sigemptyset(&sa.sa_mask);
+
+    if (sigaction(SIGUSR1, &sa, NULL) == -1) {
+        perror("sigaction");
+        return EXIT_FAILURE;
+    }
+
+    char *endptr;
+    long pid = strtol(argv[1], &endptr, 10);
+    if (*endptr != '\0' || pid <= 0) {
+        fprintf(stderr, "Invalid PID\n");
+        return EXIT_FAILURE;
+    }
+
+    char *message = argv[2];
+    while (*message) {
+        send_char(pid, *message++);
+    }
+    send_char(pid, '\0');
+
+    return EXIT_SUCCESS;
 }
