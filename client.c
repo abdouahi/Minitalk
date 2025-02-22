d0c5b1bde7f4360b250bf123aa281de01cc21235
Abdessamad ouahidi <137984861+abdouahi@users.noreply.github.com>
2025-02-22 23:57:52 +0100

    Update client.c

--- client.c
+++ client.c
@@ -3,30 +3,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static volatile sig_atomic_t g_ack = 0;
+static volatile sig_atomic_t g_ack_received = 0;
 
 static void ack_handler(int sig) {
     (void)sig;
-    g_ack = 1;
+    g_ack_received = 1;
 }
 
 static void send_bit(pid_t pid, int bit) {
-    g_ack = 0;
-    if (kill(pid, bit ? SIGUSR2 : SIGUSR1) == -1)
+    int signal = bit ? SIGUSR2 : SIGUSR1;
+    g_ack_received = 0;
+
+    if (kill(pid, signal) == -1) {
+        write(STDERR_FILENO, "Kill error\n", 11);
         exit(EXIT_FAILURE);
-    while (!g_ack)
+    }
+
+    while (!g_ack_received) {
         pause();
+    }
 }
 
 static void send_char(pid_t pid, char c) {
     for (int i = 0; i < 8; i++) {
-        send_bit(pid, (c >> i) & 1);
+        int bit = (c >> i) & 1;
+        send_bit(pid, bit);
     }
 }
 
 int main(int argc, char **argv) {
     if (argc != 3) {
-        write(2, "Usage: ./client <server_pid> <message>\n", 38);
+        write(STDERR_FILENO, "Usage: ./client PID message\n", 27);
         return 1;
     }
 
@@ -34,13 +41,19 @@ int main(int argc, char **argv) {
     sa.sa_handler = ack_handler;
     sa.sa_flags = 0;
     sigemptyset(&sa.sa_mask);
-    sigaction(SIGUSR1, &sa, NULL);
+
+    if (sigaction(SIGUSR1, &sa, NULL) == -1) {
+        write(STDERR_FILENO, "Sigaction error\n", 16);
+        return 1;
+    }
 
     pid_t server_pid = atoi(argv[1]);
     char *message = argv[2];
 
-    while (*message)
-        send_char(server_pid, *message++);
+    while (*message) {
+        send_char(server_pid, *message);
+        message++;
+    }
     send_char(server_pid, '\0');
     return 0;
 }
