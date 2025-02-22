41ddd5159c4917a8806fa1ff2ae4dc5f25f40422
Abdessamad ouahidi <137984861+abdouahi@users.noreply.github.com>
2025-02-22 06:07:22 +0100

    Update client.c

--- client.c
+++ client.c
@@ -4,33 +4,24 @@
 #include <stdio.h>
 
 static void send_bit(pid_t pid, int bit) {
-    if (bit) {
-        if (kill(pid, SIGUSR2) == -1) exit(EXIT_FAILURE);
-    } else {
-        if (kill(pid, SIGUSR1) == -1) exit(EXIT_FAILURE);
-    }
-    usleep(100);
+    if (kill(pid, bit ? SIGUSR2 : SIGUSR1) == -1) exit(1);
+    usleep(100); // Prevent signal flooding
 }
 
 static void send_char(pid_t pid, char c) {
-    int bit_pos = 7;
-    while (bit_pos >= 0) {
-        send_bit(pid, (c >> bit_pos) & 1);
-        bit_pos--;
+    for (int i = 7; i >= 0; i--) {
+        send_bit(pid, (c >> i) & 1);
     }
 }
 
 int main(int argc, char **argv) {
     if (argc != 3) {
-        write(STDERR_FILENO, "Usage: ./client <PID> <message>\n", 31);
+        fprintf(stderr, "Usage: %s <PID> <message>\n", argv[0]);
         return (1);
     }
     pid_t pid = atoi(argv[1]);
     char *str = argv[2];
-    while (*str) {
-        send_char(pid, *str);
-        str++;
-    }
+    while (*str) send_char(pid, *str++);
     send_char(pid, '\0');
     return (0);
 }
