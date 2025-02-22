bb36b5bc768fc05d7aca8babaaa2d3288f070492
Abdessamad ouahidi <137984861+abdouahi@users.noreply.github.com>
2025-02-22 05:58:50 +0100

    Update client_bonus.c

--- client_bonus.c
+++ client_bonus.c
@@ -5,46 +5,32 @@
 
 static volatile sig_atomic_t g_ack_received = 0;
 
-static void ack_handler(int sig)
-{
+static void ack_handler(int sig) {
     (void)sig;
     g_ack_received = 1;
 }
 
-static void send_bit(pid_t pid, int bit)
-{
+static void send_bit(pid_t pid, int bit) {
     g_ack_received = 0;
-    if (bit)
-    {
-        if (kill(pid, SIGUSR2) == -1)
-            exit(1);
+    if (bit) {
+        if (kill(pid, SIGUSR2) == -1) exit(EXIT_FAILURE);
+    } else {
+        if (kill(pid, SIGUSR1) == -1) exit(EXIT_FAILURE);
     }
-    else
-    {
-        if (kill(pid, SIGUSR1) == -1)
-            exit(1);
-    }
-    while (!g_ack_received)
-        pause();
+    while (!g_ack_received) pause();
 }
 
-static void send_char(pid_t pid, char c)
-{
-    int bit_pos;
-
-    bit_pos = 7;
-    while (bit_pos >= 0)
-    {
+static void send_char(pid_t pid, char c) {
+    int bit_pos = 7;
+    while (bit_pos >= 0) {
         send_bit(pid, (c >> bit_pos) & 1);
         bit_pos--;
     }
 }
 
-int main(int argc, char **argv)
-{
-    if (argc != 3)
-    {
-        fprintf(stderr, "Usage: %s <PID> <message>\n", argv[0]);
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        write(STDERR_FILENO, "Usage: ./client_bonus <PID> <message>\n", 38);
         return (1);
     }
     struct sigaction sa;
@@ -52,10 +38,10 @@ int main(int argc, char **argv)
     sa.sa_flags = 0;
     sigemptyset(&sa.sa_mask);
     sigaction(SIGUSR1, &sa, NULL);
+
     pid_t pid = atoi(argv[1]);
     char *str = argv[2];
-    while (*str)
-    {
+    while (*str) {
         send_char(pid, *str);
         str++;
     }
