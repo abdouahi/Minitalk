6f63bae84c606bcd64989dc72ad71ae88835c924
Abdessamad ouahidi <137984861+abdouahi@users.noreply.github.com>
2025-02-22 05:57:50 +0100

    Update client.c

--- client.c
+++ client.c
@@ -3,44 +3,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static void send_bit(pid_t pid, int bit)
-{
-    if (bit)
-    {
-        if (kill(pid, SIGUSR2) == -1)
-            exit(1);
-    }
-    else
-    {
-        if (kill(pid, SIGUSR1) == -1)
-            exit(1);
+static void send_bit(pid_t pid, int bit) {
+    if (bit) {
+        if (kill(pid, SIGUSR2) == -1) exit(EXIT_FAILURE);
+    } else {
+        if (kill(pid, SIGUSR1) == -1) exit(EXIT_FAILURE);
     }
     usleep(100);
 }
 
-static void send_char(pid_t pid, char c)
-{
-    int bit_pos;
-
-    bit_pos = 7;
-    while (bit_pos >= 0)
-    {
+static void send_char(pid_t pid, char c) {
+    int bit_pos = 7;
+    while (bit_pos >= 0) {
         send_bit(pid, (c >> bit_pos) & 1);
         bit_pos--;
     }
 }
 
-int main(int argc, char **argv)
-{
-    if (argc != 3)
-    {
-        fprintf(stderr, "Usage: %s <PID> <message>\n", argv[0]);
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        write(STDERR_FILENO, "Usage: ./client <PID> <message>\n", 31);
         return (1);
     }
     pid_t pid = atoi(argv[1]);
     char *str = argv[2];
-    while (*str)
-    {
+    while (*str) {
         send_char(pid, *str);
         str++;
     }
