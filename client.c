f73a9f354afcfed65d534f68ab13725452c25288
Abdessamad ouahidi <137984861+abdouahi@users.noreply.github.com>
2025-02-23 00:09:58 +0100

    Update client.c

--- client.c
+++ client.c
@@ -2,58 +2,73 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
 
-static volatile sig_atomic_t g_ack_received = 0;
+static volatile sig_atomic_t g_ack = 0;
 
-static void ack_handler(int sig) {
+static void ack_handler(int sig)
+{
     (void)sig;
-    g_ack_received = 1;
+    g_ack = 1;
 }
 
-static void send_bit(pid_t pid, int bit) {
-    int signal = bit ? SIGUSR2 : SIGUSR1;
-    g_ack_received = 0;
-
-    if (kill(pid, signal) == -1) {
-        write(STDERR_FILENO, "Kill error\n", 11);
+static void send_bit(pid_t pid, int bit)
+{
+    int signal;
+    
+    g_ack = 0;
+    if (bit)
+        signal = SIGUSR2;
+    else
+        signal = SIGUSR1;
+    
+    if (kill(pid, signal) == -1)
+    {
+        write(STDERR_FILENO, "Error: Invalid PID\n", 18);
         exit(EXIT_FAILURE);
     }
-
-    while (!g_ack_received) {
+    while (!g_ack)
         pause();
-    }
 }
 
-static void send_char(pid_t pid, char c) {
-    for (int i = 0; i < 8; i++) {
+static void send_char(pid_t pid, char c)
+{
+    int i;
+    
+    i = 0;
+    while (i < 8)
+    {
         int bit = (c >> i) & 1;
         send_bit(pid, bit);
+        i++;
     }
 }
 
-int main(int argc, char **argv) {
-    if (argc != 3) {
-        write(STDERR_FILENO, "Usage: ./client PID message\n", 27);
-        return 1;
-    }
-
+int main(int argc, char **argv)
+{
     struct sigaction sa;
-    sa.sa_handler = ack_handler;
-    sa.sa_flags = 0;
-    sigemptyset(&sa.sa_mask);
-
-    if (sigaction(SIGUSR1, &sa, NULL) == -1) {
-        write(STDERR_FILENO, "Sigaction error\n", 16);
+    long pid;
+    char *endptr;
+    
+    if (argc != 3)
+    {
+        write(STDERR_FILENO, "Usage: ./client PID MESSAGE\n", 27);
         return 1;
     }
-
-    pid_t server_pid = atoi(argv[1]);
-    char *message = argv[2];
-
-    while (*message) {
-        send_char(server_pid, *message);
-        message++;
+    
+    pid = strtol(argv[1], &endptr, 10);
+    if (*endptr != '\0' || pid <= 0 || pid > PID_MAX)
+    {
+        write(STDERR_FILENO, "Error: Invalid PID\n", 18);
+        return 1;
     }
-    send_char(server_pid, '\0');
+    
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = ack_handler;
+    sigaction(SIGUSR1, &sa, NULL);
+    
+    while (*argv[2])
+        send_char((pid_t)pid, *argv[2]++);
+    send_char((pid_t)pid, '\0');
     return 0;
 }
