60e1ff52c95c5a2b52c2cec38fa2c6b6fdda11fd
Abdessamad ouahidi <137984861+abdouahi@users.noreply.github.com>
2025-02-27 23:13:24 +0000

    V666

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,42 +6,33 @@
 /*   By: abdouahi <abdouahi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 16:03:35 by abdouahi          #+#    #+#             */
-/*   Updated: 2025/02/27 22:59:22 by abdouahi         ###   ########.fr       */
+/*   Updated: 2025/02/27 23:06:23 by abdouahi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int	is_a_digit(char *str)
+int ft_atoi(char *str)
 {
-	int	i;
-	int	j;
+    int res;
+    int i;
 
-	j = 0;
-	i = 0;
-	while ((str[i] >= '0' && str[i] <= '9'))
-		i++;
-	if (i == j)
-		return (0);
-	return (1);
-}
-
-int	ft_atoi(char *str)
-{
-	int		res;
-	int		sign;
-	int 	i;
-
-	i = 0;
-	sign = 1;
-	res = 0;
-	is_a_digit(str);
-	if ((str[i] == '-' || str[i] == '+' ))
-		exit(1);
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		res = res * 10 + str[i] - '0';
-		i++;
-	}
-	return (res * sign);
+    i = 0;
+    res = 0;
+    
+    if (str[0] == '0' && str[1] != '\0')
+        exit(1);
+        
+    if (!(str[i] >= '0' && str[i] <= '9'))
+        exit(1);
+        
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        res = res * 10 + str[i] - '0';
+        i++;
+    }
+    if (str[i] != '\0')
+        exit(1);
+        
+    return (res);
 }
